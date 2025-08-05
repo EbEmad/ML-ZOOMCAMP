82e4f4a173217113db3b1c84a674ad3074438c9b
Ebrahim Emad <ebemad8@gmail.com>
2025-08-05 11:32:11 +0300

    adding numpy

--- 01-intro/run.sh
+++ 01-intro/run.sh
@@ -1,8 +1,9 @@
-
+#!/bin/bash
 echo "Starting Jupyter Notebook..."
 jupyter notebook \
   --ip=0.0.0.0 \
   --port=8000 \
   --no-browser \
   --NotebookApp.token='' \
-  --NotebookApp.password=''
+  --NotebookApp.password='' \
+  --allow-root
